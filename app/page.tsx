6269f2f59c3b6c899ef9c9dba4946ddcf4d6a2d1
tomas-oa <tomas.opazoasd@gmail.com>
2025-05-04 00:28:13 -0400

    feat: add projects section

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,11 @@ import {
   HREF_GITHUB,
   HREF_LINKEDIN,
   HREF_MAIL,
+  PROJECTS,
   TAGS,
 } from "@/constants";
 
+import arrow from "@/public/arrow.svg";
 import sticker from "@/public/sticker.svg";
 
 import lets_work_mobile from "@/public/lets-work/lets_work_mobile.png";
@@ -27,6 +29,77 @@ import footer_smiley from "@/public/footer/smiley.svg";
 export default function RootPage() {
   return (
     <main className={clsx("grid grid-cols-1")}>
+      <section
+        id="proyectos"
+        className={clsx("flex flex-col divide-y divide-black border-y")}
+      >
+        {PROJECTS.map((project) => (
+          <Link
+            href={`/proyectos/${project["slug"]}`}
+            key={project["slug"]}
+            className={clsx(
+              "flex flex-col divide-y divide-black",
+              "lg:divide-none lg:flex-row lg:justify-between",
+            )}
+          >
+            <div
+              className={clsx(
+                "flex flex-row items-center justify-between px-container py-7",
+                "lg:w-full",
+              )}
+            >
+              <div className={clsx("flex flex-row items-center gap-5")}>
+                <div
+                  className={clsx(
+                    project?.["color"],
+                    "aspect-square size-5 border",
+                  )}
+                />
+
+                <h2
+                  className={clsx(
+                    "w-3/4 text-balance font-display font-medium text-3xl text-black leading-6",
+                    "sm:w-full",
+                    "lg:text-4xl/8",
+                  )}
+                >
+                  {project?.["title"]}
+                </h2>
+              </div>
+
+              <div className={clsx("flex flex-col items-end", "lg:hidden")}>
+                <p className={clsx("flex font-light text-black leading-4")}>
+                  {project?.["tags"].map((tag) => tag?.["label"]).join(", ")}
+                </p>
+              </div>
+            </div>
+
+            <div
+              className={clsx(
+                "hidden",
+                "lg:px-container lg:py-7 lg:flex lg:flex-row lg:justify-between lg:items-center lg:gap-16 lg:w-full",
+              )}
+            >
+              <p className={clsx("flex font-light text-black leading-4")}>
+                {project?.["tags"].map((tag) => tag?.["label"]).join(", ")}
+              </p>
+
+              <Image
+                src={arrow}
+                alt="arrow"
+                className={clsx("size-10 aspect-square rotate-180")}
+              />
+            </div>
+
+            <Image
+              className={clsx("lg:hidden")}
+              src={project["storage"]["cover"]}
+              alt={project["title"]}
+            />
+          </Link>
+        ))}
+      </section>
+
       <section
         id="trabajemos"
         className={clsx("relative flex h-150 flex-col", "lg:hidden")}
--- constants.ts
+++ constants.ts
@@ -1,3 +1,27 @@
+import una_ventana_para_el_arte_1 from "@/public/una-ventana-para-el-arte/1.png";
+import una_ventana_para_el_arte_2 from "@/public/una-ventana-para-el-arte/2.png";
+import una_ventana_para_el_arte_3 from "@/public/una-ventana-para-el-arte/3.png";
+import una_ventana_para_el_arte_4 from "@/public/una-ventana-para-el-arte/4.png";
+import una_ventana_para_el_arte_5 from "@/public/una-ventana-para-el-arte/5.png";
+import una_ventana_para_el_arte_6 from "@/public/una-ventana-para-el-arte/6.png";
+import una_ventana_para_el_arte_7 from "@/public/una-ventana-para-el-arte/7.png";
+import una_ventana_para_el_arte_8 from "@/public/una-ventana-para-el-arte/8.png";
+import una_ventana_para_el_arte_cover from "@/public/una-ventana-para-el-arte/cover.png";
+
+import kombucha_loyca_1 from "@/public/kombucha-loyca/1.png";
+import kombucha_loyca_2 from "@/public/kombucha-loyca/2.png";
+import kombucha_loyca_3 from "@/public/kombucha-loyca/3.png";
+import kombucha_loyca_4 from "@/public/kombucha-loyca/4.png";
+import kombucha_loyca_5 from "@/public/kombucha-loyca/5.png";
+import kombucha_loyca_cover from "@/public/kombucha-loyca/cover.png";
+
+import frankenstein_1 from "@/public/frankenstein/1.png";
+const frankenstein_2 = "/frankenstein/2.mp4";
+import frankenstein_3 from "@/public/frankenstein/3.png";
+import frankenstein_4 from "@/public/frankenstein/4.gif";
+import frankenstein_5 from "@/public/frankenstein/5.png";
+import frankenstein_cover from "@/public/frankenstein/cover.png";
+
 export const HREF_MAIL = "mailto:isidoramorenoesquivel@gmail.com";
 export const HREF_LINKEDIN = "https://www.linkedin.com/in/isidoramoreno/";
 export const HREF_BEHANCE = "https://www.behance.net/doradisenadora";
@@ -5,28 +29,446 @@ export const HREF_CV = "/cv.pdf";
 export const HREF_GITHUB = "https://github.com/tomas-oa";
 
 export function NOW() {
-	return new Date();
+  return new Date();
 }
 
 export const TAGS = {
-	design: {
-		label: "Diseño integral",
-		className: "bg-black text-white",
-	},
-	graphic: {
-		label: "Gráfico",
-		className: "bg-dora-sky text-black",
-	},
-	editorial: {
-		label: "Editorial",
-		className: "bg-dora-pink text-black",
-	},
-	branding: {
-		label: "Branding",
-		className: "bg-dora-red text-black",
-	},
-	illustration: {
-		label: "Ilustración",
-		className: "bg-dora-yellow text-black",
-	},
+  design: {
+    key: "design",
+    label: "Diseño integral",
+    className: "bg-black text-white",
+  },
+  graphic: {
+    key: "graphic",
+    label: "Gráfico",
+    className: "bg-dora-sky text-black",
+  },
+  editorial: {
+    key: "editorial",
+    label: "Editorial",
+    className: "bg-dora-pink text-black",
+  },
+  branding: {
+    key: "branding",
+    label: "Branding",
+    className: "bg-dora-red text-black",
+  },
+  illustration: {
+    key: "illustration",
+    label: "Ilustración",
+    className: "bg-dora-yellow text-black",
+  },
 } as const;
+
+export const PROJECTS = [
+  {
+    title: "Una ventana para el arte",
+    slug: "una-ventana-para-el-arte",
+    color: "bg-dora-pink",
+    content: {
+      diggest:
+        "Sistema editorial para el Museo de Artes Visuales MAVI UC, que promueve una experiencia museográfica reflexiva e interactiva.",
+      summary: `Impulsado por el Departamento de Educación e Inclusión del MAVI UC, este proyecto buscó transformar la experiencia de los visitantes mediante folletos que promovieran una interacción reflexiva con las obras.
+
+Ante el desafío de producir un material atractivo, adaptable y eficiente, se implementó un sistema gráfico que preservaba la identidad visual del museo, pero con suficiente flexibilidad para adaptarse a la estética particular de cada exposición. Esta solución optimizó los procesos de diseño y distribución, consolidándose como un formato replicable que se materializó con tres exposiciones que utilizaron este modelo; sentando las bases para futuras ediciones.`,
+    },
+    tags: [
+      {
+        key: TAGS?.["editorial"]?.["key"],
+        label: TAGS?.["editorial"]?.["label"],
+        className: TAGS?.["editorial"]?.["className"],
+      },
+    ],
+    storage: {
+      cover: una_ventana_para_el_arte_cover,
+      images: [
+        {
+          src: una_ventana_para_el_arte_1,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_2,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_3,
+          alt: "Una ventana para el arte",
+          className: "xl:scale-108",
+        },
+        {
+          src: una_ventana_para_el_arte_4,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_5,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "Kombucha Loyca",
+    slug: "kombucha-loyca",
+    color: "bg-dora-orange",
+    content: {
+      diggest:
+        "Rebranding estratégico para reposicionar una bebida artesanal en el mercado gastronómico contemporáneo.",
+      summary: `Loyca impulsó un rebranding estratégico con el objetivo de aumentar su alcance en el mercado, reposicionando su kombucha más allá del nicho orgánico convencional, para consolidarse como una alternativa versátil y gourmet en el mercado gastronómico; orientándose a restaurantes, coctelería de autor y cafés de especialidad. 
+
+El nuevo enfoque de identidad mantiene la esencia artesanal de Loyca, combinándola con un carácter contemporáneo, fresco y experimental. Este replanteamiento fortaleció la identidad de la marca, facilitando también su integración en nuevos contextos de consumo, aumentando su percepción de calidad, autenticidad y adaptabilidad.`,
+    },
+    tags: [
+      {
+        key: TAGS?.["branding"]?.["key"],
+        label: TAGS?.["branding"]?.["label"],
+        className: TAGS?.["branding"]?.["className"],
+      },
+    ],
+    storage: {
+      cover: kombucha_loyca_cover,
+      images: [
+        {
+          src: kombucha_loyca_1,
+          alt: "Kombucha Loyca",
+          className: "",
+        },
+        {
+          src: kombucha_loyca_2,
+          alt: "Kombucha Loyca",
+          className: "",
+        },
+        {
+          src: kombucha_loyca_3,
+          alt: "Kombucha Loyca",
+          className: "",
+        },
+        {
+          src: kombucha_loyca_4,
+          alt: "Kombucha Loyca",
+          className: "",
+        },
+        {
+          src: kombucha_loyca_5,
+          alt: "Kombucha Loyca",
+          className: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "Chao Reunionitis",
+    slug: "chao-reunionitis",
+    color: "bg-dora-green",
+    content: {
+      diggest:
+        "Campaña gráfica interna para Teck, orientada a promover una gestión eficiente de las reuniones de trabajo.",
+      summary: `Frente a la problemática del exceso de reuniones y su impacto en la productividad y el bienestar de los equipos, un equipo de liderazgo interno de Teck impulsó una campaña de sensibilización orientada a promover una gestión más eficiente del tiempo durante las reuniones de trabajo.
+
+La campaña tomó como eje conceptual la "reunionitis", un término humorístico que permitió abordar el tema de forma cercana y amena, facilitando la identificación y reflexión de los colaboradores. A partir de este concepto se desarrollaron piezas gráficas ilustradas que, mediante el humor, transmitieron buenas prácticas para planificar y asistir a reuniones de manera más efectiva. La estrategia se complementó con acciones de difusión interna que reforzaron el mensaje en distintos espacios de trabajo, favoreciendo la adopción de comportamientos más eficientes.      `,
+    },
+    tags: [
+      {
+        key: TAGS?.["graphic"]?.["key"],
+        label: TAGS?.["graphic"]?.["label"],
+        className: TAGS?.["graphic"]?.["className"],
+      },
+      {
+        key: TAGS?.["illustration"]?.["key"],
+        label: TAGS?.["illustration"]?.["label"],
+        className: TAGS?.["illustration"]?.["className"],
+      },
+    ],
+    storage: {
+      cover: una_ventana_para_el_arte_cover,
+      images: [
+        {
+          src: una_ventana_para_el_arte_1,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_2,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_3,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_4,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_5,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_6,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_7,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_8,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "Frankenstein",
+    slug: "frankenstein",
+    color: "bg-dora-sky",
+    content: {
+      diggest:
+        "Afiche cultural desarrollado como proyecto universitario para conmemorar los 200 años de Frankenstein.",
+      summary: `Proyecto universitario de difusión cultural, desarrollado para conmemorar los más de 200 años de la publicación de Frankenstein (1818) y acercar su legado a nuevas generaciones.  El afiche se centra en reinterpretar una de las principales tensiones de la obra: la contraposición entre la bondad del monstruo y la maldad del ser humano.
+
+Bajo este concepto, la propuesta invita a una lectura contemporánea de la obra, abordando los dilemas éticos y existenciales que plantea. La propuesta utiliza recursos visuales metafóricos para enriquecer la interpretación y promover una conexión crítica y emocional con el espectador contemporáneo.`,
+    },
+    tags: [
+      {
+        key: TAGS?.["graphic"]?.["key"],
+        label: TAGS?.["graphic"]?.["label"],
+        className: TAGS?.["graphic"]?.["className"],
+      },
+    ],
+    storage: {
+      cover: frankenstein_cover,
+      images: [
+        {
+          src: frankenstein_1,
+          alt: "Frankenstein",
+          className: "",
+        },
+        {
+          src: frankenstein_2,
+          alt: "Frankenstein",
+          className: "",
+        },
+        {
+          src: frankenstein_3,
+          alt: "Frankenstein",
+          className: "",
+        },
+        {
+          src: frankenstein_4,
+          alt: "Frankenstein",
+          className: "",
+        },
+        {
+          src: frankenstein_5,
+          alt: "Frankenstein",
+          className: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "A solas, el jardín",
+    slug: "a-solas-el-jardin",
+    color: "bg-dora-violet",
+    content: {
+      diggest:
+        "Publicación editorial ilustrada que aborda la soledad emocional en jóvenes a través de una metáfora visual y relatos personales.",
+      summary: `Proyecto de título enfocado en la problemática de la soledad emocional en personas jóvenes, desarrollado a través de una publicación editorial ilustrada. El proyecto utiliza representaciones metafóricas de experiencias cotidianas para abrir espacios de interpretación e identificación personal, acompañadas de relatos anónimos que invitan a la reflexión y al ejercicio introspectivo.
+
+La propuesta busca generar una experiencia de lectura sensible y auténtica, combinando una atmósfera visual introspectiva con el carácter crudo de las vivencias narradas. El desarrollo del proyecto implicó un extenso proceso de investigación, entrevistas personales, iteración de contenidos y validaciones cualitativas, con el objetivo de construir un dispositivo editorial capaz de dialogar de manera genuina con las emociones del lector.`,
+    },
+    tags: [
+      {
+        key: TAGS?.["editorial"]?.["key"],
+        label: TAGS?.["editorial"]?.["label"],
+        className: TAGS?.["editorial"]?.["className"],
+      },
+      {
+        key: TAGS?.["illustration"]?.["key"],
+        label: TAGS?.["illustration"]?.["label"],
+        className: TAGS?.["illustration"]?.["className"],
+      },
+    ],
+    storage: {
+      cover: una_ventana_para_el_arte_cover,
+      images: [
+        {
+          src: una_ventana_para_el_arte_1,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_2,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_3,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_4,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_5,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_6,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_7,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_8,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "¿?",
+    slug: "¿?",
+    color: "bg-dora-pink",
+    content: {
+      diggest: "",
+      summary: "",
+    },
+    tags: [
+      {
+        key: TAGS?.["editorial"]?.["key"],
+        label: TAGS?.["editorial"]?.["label"],
+        className: TAGS?.["editorial"]?.["className"],
+      },
+    ],
+    storage: {
+      cover: una_ventana_para_el_arte_cover,
+      images: [
+        {
+          src: una_ventana_para_el_arte_1,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_2,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_3,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_4,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_5,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_6,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_7,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_8,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "Dora Ilustra",
+    slug: "dora-ilustra",
+    color: "bg-dora-yellow",
+    content: {
+      diggest:
+        "Proyecto personal de ilustración centrado en la creación de personajes, exploración emocional y el desarrollo de un lenguaje visual propio.",
+      summary: `Dora Ilustra es un proyecto personal de ilustración enfocado en la creación de personajes y el desarrollo de un lenguaje visual propio. Basado en la expresión emocional como eje conceptual, explora formas de representación que, a través de una gráfica sólida y distintiva, buscan generar conexión e interpretación en distintas audiencias.
+
+El proyecto funciona como un espacio de experimentación visual y narrativa, que impulsa el crecimiento creativo y la consolidación de una identidad artística en constante evolución.`,
+    },
+    tags: [
+      {
+        key: TAGS?.["illustration"]?.["key"],
+        label: TAGS?.["illustration"]?.["label"],
+        className: TAGS?.["illustration"]?.["className"],
+      },
+    ],
+    storage: {
+      cover: una_ventana_para_el_arte_cover,
+      images: [
+        {
+          src: una_ventana_para_el_arte_1,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_2,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_3,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_4,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_5,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_6,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_7,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+        {
+          src: una_ventana_para_el_arte_8,
+          alt: "Una ventana para el arte",
+          className: "",
+        },
+      ],
+    },
+  },
+] as const;
