46ac089089f2a61e92d746bdebe9c04232c5131a
tomas-oa <tomas.opazoasd@gmail.com>
2025-05-03 21:48:40 -0400

    feat: add constants

---
+++ constants.ts
@@ -0,0 +1,9 @@
+export const HREF_MAIL = "mailto:isidoramorenoesquivel@gmail.com";
+export const HREF_LINKEDIN = "https://www.linkedin.com/in/isidoramoreno/";
+export const HREF_BEHANCE = "https://www.behance.net/doradisenadora";
+export const HREF_CV = "/cv.pdf";
+export const HREF_GITHUB = "https://github.com/tomas-oa";
+
+export function NOW() {
+	return new Date();
+}
